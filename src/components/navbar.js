c53e32346f9f174ad6171eccb6dca5302a3a8e16
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-03-11 15:18:15 +0800

    Created the layout and navbar components.

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../images/pflegeteam-pinneberg-logo.svg";
+
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <Logo className="logo" />
+    </div>
+  )
+};
+
+export default Navbar;
