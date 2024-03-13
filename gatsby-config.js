c078653384f2bfdee7a98bdb82577e867258c4b2
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-03-13 18:23:45 +0800

    Created the Footer component.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,9 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`, 
     `gatsby-plugin-react-svg`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,68 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import Logo from "../../images/pflegeteam-pinneberg-logo-white.svg";
+
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div className="row">
+        <div className="col col-1">
+          <h3>
+            Für ein sorgenfreies Leben mit Pflegeteam-Pinneberg
+          </h3>
+          <div className="services-wrapper">
+            <h5>
+              UNSERE LEISTUNGEN SEHEN
+            </h5>
+          </div>
+        </div>
+        <div className="col col-2">
+          <h6>Nützliche Links</h6>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/leistungen">Leistungen</Link>
+            <Link to="/ueber-uns">Über uns</Link>
+            <Link to="/karriere">Karriere</Link>
+            <Link to="/kontakte">Kontakte</Link>
+          </nav>
+        </div>
+        <div className="col col-3">
+          <div className="contact-wrapper">
+            <h6>Kontakt</h6>
+            <p>
+              Tel.:&nbsp;
+              <a href="">
+                04101 – 604 60 91
+              </a>
+            </p>
+            <p>
+              Fax:&nbsp;
+              <a href="">
+                04101 – 604 76 95
+              </a>
+            </p>
+            <p>
+              Email:&nbsp;
+              <a href="">
+                info@pflegeteam-pinneberg.de
+              </a>
+            </p>
+          </div>
+          <p className="address">
+            Dingstätte 1825421
+            <br />
+            Pinneberg
+            <br />
+            Deutschland
+          </p>
+        </div>
+        <div className="col col-4">
+          <Logo className="logo"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
