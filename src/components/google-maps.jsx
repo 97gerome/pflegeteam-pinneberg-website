a0e7acdeb771cad486ef311385628e665bdb6b23
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-04-05 17:04:58 +0800

    Added controlled prop.

--- src/components/google-maps.jsx
+++ src/components/google-maps.jsx
@@ -1,10 +1,16 @@
-import { APIProvider, AdvancedMarker, Map } from '@vis.gl/react-google-maps';
+import {
+  APIProvider,
+  AdvancedMarker,
+  ControlPosition,
+  Map,
+  MapControl,
+} from '@vis.gl/react-google-maps';
 import React from 'react';
 
 const apiKey = process.env.GATSBY_GOOGLE_MAPS_API_KEY;
 const mapId = process.env.GATSBY_MAP_ID;
 
-const GoogleMaps = ({ coordinates }) => {
+const GoogleMaps = ({ coordinates, controlled }) => {
   return (
     <APIProvider apiKey={apiKey}>
       <Map
@@ -13,6 +19,13 @@ const GoogleMaps = ({ coordinates }) => {
         defaultZoom={15}
         gestureHandling={'greedy'}
         mapId={mapId}
+        controlled={controlled}
+        center={controlled ? coordinates : null}
+        zoom={controlled ? 15 : null}
+        fullscreenControl={false}
+        mapTypeControl={false}
+        zoomControl={controlled ? false : true}
+        streetViewControl={controlled ? false : true}
       >
         <AdvancedMarker position={coordinates} />
       </Map>
