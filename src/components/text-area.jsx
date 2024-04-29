57307d83ce934ed82e0f048bfcbe67d222c9efac
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-04-29 19:06:10 +0800

    Added the value, error and disabled props.

--- src/components/text-area.jsx
+++ src/components/text-area.jsx
@@ -1,17 +1,30 @@
+import clsx from 'clsx';
 import React from 'react';
 
-const TextArea = ({ id, label, onChange = () => {}, rows, columns }) => {
+const TextArea = ({
+  id,
+  label,
+  value,
+  error,
+  onChange = () => {},
+  disabled,
+  rows,
+  columns,
+}) => {
   return (
-    <div id={id} className="text-area">
+    <div id={id} className={clsx({ 'text-area': true, error: error })}>
       <label>
         {label}
         <textarea
           type="text"
+          value={value}
           onChange={onChange}
           rows={rows}
           columns={columns}
+          disabled={disabled}
         />
       </label>
+      <p className="error-text extra-small">{error}</p>
     </div>
   );
 };
