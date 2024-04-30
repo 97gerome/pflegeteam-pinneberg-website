8af9a16b238b0ec589d1ec20ac58babc7be02017
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-04-30 18:08:28 +0800

    Added the onChange, error, accept and disabled props.

--- src/components/file-input.jsx
+++ src/components/file-input.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import HochladenIcon from '../images/hochladen-icon.svg';
+import clsx from 'clsx';
 
-const FileInput = ({ id, label }) => {
+const FileInput = ({ id, label, onChange, error, accept, disabled }) => {
   return (
-    <div id={id} className="file-input">
+    <div id={id} className={clsx({ 'file-input': true, error: error })}>
       <label>
         {label}
-        <div className="input-wrapper">
+        <div className={clsx({ 'input-wrapper': true, disabled: disabled })}>
           <HochladenIcon />
           <p>Datei Hochladen</p>
-          <input type="file" />
+          <input
+            type="file"
+            onChange={onChange}
+            accept={accept}
+            disabled={disabled}
+          />
         </div>
       </label>
+      <p className="error-text extra-small">{error}</p>
     </div>
   );
 };
